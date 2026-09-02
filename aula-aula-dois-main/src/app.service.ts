45c434f1baafc483278c957d9ca64c7443b001c0
GustavoMorchelIENH <gustavomorchel@ienh.com.br>
2026-09-02 14:10:12 -0300

    terceiro commit

--- aula-aula-dois-main/src/app.service.ts
+++ aula-aula-dois-main/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Agora foi sa merda do krl poha backend de merda do krl poha';
   }
 }
